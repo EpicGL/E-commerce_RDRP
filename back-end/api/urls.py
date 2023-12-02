8610353b8a5a59f61419b49af65cae344f7e254e
mukit hasan <mukithasan58@gmail.com>
2023-12-02 22:43:34 +0600

    cart

--- back-end/api/urls.py
+++ back-end/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import productViews
+from .views import cartViews
 from myauth.auth_views import MyTokenObtainPairSerializer
 from myauth import auth_views
 from rest_framework_simplejwt.views import (
@@ -13,8 +14,14 @@ urlpatterns = [
     path('product/<str:slug>', productViews.get_product, name='product'),
     path('products/filter/', productViews.get_filter_product, name='filter_product'),
 
+    # Auth login, verify token and refresh token
     path('token/', TokenObtainPairView.as_view(
         serializer_class=MyTokenObtainPairSerializer), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', auth_views.verify, name='verify_token')
+    path('token/verify/', auth_views.verify, name='verify_token'),
+
+
+    path('cart/', cartViews.add_or_to_cart, name='add_or_to_cart'),
+
+
 ]
--- back-end/api/views/cartViews.py
+++ back-end/api/views/cartViews.py
@@ -3,3 +3,54 @@ from rest_framework.decorators import api_view
 from product.models import Product
 from cart.models import Order, OrderItem
 from .serializers import ProductSerializer
+
+
+@api_view(['GET'])
+def add_or_to_cart(request):
+    orderId = request.query_params.get('order')
+    action = request.query_params.get('action')
+    productId = request.query_params.get('product')
+
+    try:
+        product = Product.objects.get(id=productId)
+    except:
+        return Response({"No Product Found with this id "})
+
+    try:
+        if orderId:
+            try:
+                order = Order.objects.get(id=orderId)
+            except Order.DoesNotExist:
+                order = Order.objects.create()
+        else:
+            order = Order.objects.create()
+
+        existing_order_item = OrderItem.objects.filter(
+            item=productId, order=order).first()
+
+        try:
+            if existing_order_item:
+                if action == 'increment':
+                    existing_order_item.quantity += 1
+                    existing_order_item.save()
+                    order_item = existing_order_item
+                    return Response({"Item added"})
+                elif action == 'decrement':
+                    if existing_order_item > 1:
+                        existing_order_item.quantity -= 1
+                        existing_order_item.save()
+                        order_item = existing_order_item
+                        return Response({"Item removed"})
+                    else:
+                        existing_order_item.delete()
+                        return Response({"Item was removed from cart"})
+                else:
+                    return Response({"wrong action"})
+            else:
+                order_item = OrderItem.objects.create(
+                    order=order, item=product)
+                return Response({'Order'}, status=200)
+        except:
+            return Response({"fuck this shit"})
+    except:
+        return Response({'no product'}, status=404)
