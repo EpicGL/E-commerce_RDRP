ea371d2fbc29724726f43b33ba63b216bcf55cf0
mukit hasan <mukithasan58@gmail.com>
2023-12-09 23:18:24 +0600

    fix somethings :)

--- OLD Files/back-end/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import productViews
-
-urlpatterns = [
-    path('products/', productViews.get_products, name='products'),
-    path('product/<str:slug>', productViews.get_product, name='product'),
-    path('products/filter/', productViews.get_filter_product, name='filter_product')
-]
--- OLD Files/back-end/api/views/cartViews.py
+++
@@ -1,4 +0,0 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from .serializers import ProductSerializer
-from product.models import Product
